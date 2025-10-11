1293537c427516df146de9b5453a1a5a8d978672
David Hirsch <dhirsch1138@gmail.com>
2025-10-11 18:58:45 -0400

    first commit

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    #nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+  let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in
+  {
+    nixosConfigurations = {
+      helios = nixpkgs.lib.nixosSystem {
+        specialArgs = {
+          inherit system;
+        };
+        modules = [
+        ./helios/configuration.nix
+        ];
+      };
+    };
+  };
+}
