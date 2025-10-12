92e33515fbf1499035749574a68629806051ff7d
David Hirsch <dhirsch1138@gmail.com>
2025-10-11 22:04:41 -0400

    so flakes only see files in git, got it

---
+++ copyHwConfig_helios.sh
@@ -0,0 +1,2 @@
+rm ./helios/hardware-configuration.nix
+cp /etc/nixos/hardware-configuration.nix ./helios/hardware-configuration.nix
--- symlinkHwConfig_helios.sh
+++ symlinkHwConfig_helios.sh
@@ -1,3 +1,2 @@
-ln --backup -s /etc/nixos/hardware-configuration.nix ./helios/hardware-configuration.nix
-#rm ./helios/hardware-configuration.nix
-#cp /etc/nixos/hardware-configuration.nix ./helios/hardware-configuration.nix
+#ln --backup -s /etc/nixos/hardware-configuration.nix ./helios/hardware-configuration.nix
+echo this does nothing
