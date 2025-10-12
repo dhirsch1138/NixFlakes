ae2a7309ff4969508ebbc2e77ceaa01000c842f4
David Hirsch <dhirsch1138@gmail.com>
2025-10-11 22:23:16 -0400

    trying to move crypt id to its own file x 2

--- helios/configuration.nix
+++ helios/configuration.nix
@@ -8,7 +8,7 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
-      ./manual_CryptDevices.nix
+      ./manual_cryptDevices.nix
     ];
 
   # Enable flakes
