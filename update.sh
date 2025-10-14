3b0632af921119a10203623de464018c6e96776e
David Hirsch <dhirsch1138@gmail.com>
2025-10-14 17:51:59 -0400

    testing out having update script report diffs that upgrade will apply

--- update.sh
+++ update.sh
@@ -1,2 +1,13 @@
+hs=$(hostname)
 echo updating flake.nix in $(pwd)
+echo -----------
+echo calling "nix flake update"
 nix flake update
+echo update complete
+echo building derivation to determine changes
+nixos-rebuild build --flake ./#$hs
+echo derivation built, performing diff
+echo method 1: using "nix store diff-closures"
+nix store diff-closures /run/current-system ./result
+echo method 2: using nvm diff
+nix-shell -p nvd --run "nvd diff /run/current-system/ ./result"
